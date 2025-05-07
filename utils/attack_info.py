5ee85b07425780f50c34302bb2e8d74250399002
leeazyone <leejiwon317@naver.com>
2025-05-08 00:27:23 +0900

    create:/utils/attack_into.py

---
+++ utils/attack_info.py
@@ -0,0 +1,34 @@
+attack_explanations = {
+    "SYN Flood": {
+        "desc": "SYN Flood는 TCP 연결 요청을 대량으로 보내 서버 자원을 고갈시키는 공격입니다.",
+        "level": "높음"
+    },
+    "Port Scan": {
+        "desc": "Port Scan은 여러 포트를 스캔하여 열린 포트를 찾으려는 정찰 행위입니다.",
+        "level": "낮음"
+    },
+    "Ping of Death": {
+        "desc": "Ping of Death는 비정상적으로 큰 ping 패킷을 보내 시스템을 다운시키는 공격입니다.",
+        "level": "높음"
+    },
+    "ICMP Flood": {
+        "desc": "ICMP Flood는 다량의 ICMP Echo 요청(ping)을 보내서 대상 서버를 과부하시키는 공격입니다.",
+        "level": "중간"
+    },
+    "UDP Flood": {
+        "desc": "UDP Flood는 다량의 UDP 패킷을 무작위 포트로 보내 서버 리소스를 고갈시키는 공격입니다.",
+        "level": "중간"
+    },
+    "Smurf Attack": {
+        "desc": "Smurf Attack은 ICMP 요청을 브로드캐스트로 보내 다수 시스템이 피해자를 향해 응답하게 만드는 공격입니다.",
+        "level": "높음"
+    },
+    "ARP Spoofing": {
+        "desc": "ARP Spoofing은 위조된 ARP 메시지를 전송하여 네트워크 트래픽을 가로채거나 조작하는 공격입니다.",
+        "level": "중간"
+    },
+    "Normal Traffic": {
+        "desc": "정상적인 트래픽으로 판단됩니다.",
+        "level": "없음"
+    }
+}
