58bd972f3a5df7ad317643db1e6b215eb250fedb
leeazyone <leejiwon317@naver.com>
2025-05-08 02:11:16 +0900

    fix:/utils/logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,13 @@
 import csv
 from datetime import datetime
 
-def log_prediction(filename, attack, anomaly, level, log_file):
+def log_prediction(filename, attack, level, log_file):
   with open(log_file, 'a', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow([datetime.now(), filename, attack, anomaly, level])
+    writer.writerow([datetime.now(), filename, attack, level])
+
+# anomaly는 이상탐지 모델로 하는 것이기 때문에 지금은 제거 
+#def log_prediction(filename, attack, anomaly, level, log_file):
+#with open(log_file, 'a', newline='') as csvfile:
+#   writer = csv.writer(csvfile)
+#   writer.writerow([datetime.now(), filename, attack, anomaly, level])
