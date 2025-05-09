94ca077e7b156a11ab7dc91df34662b6ffd5fd49
leetaeyeon11111 <hahaha2662@naver.com>
2025-05-09 20:37:15 +0900

    1

--- routes/log.py
+++ routes/log.py
@@ -4,4 +4,4 @@ log_bp = Blueprint('log', __name__)
 
 @log_bp.route("/log", methods=["GET"])
 def hello_log():
-  return {"message": "여긴 /log API야. 곧 로그가 여기에 출력될거야"}
+  return {"message": "여긴 /log API야. 곧 로그가 여ddddd기에 출력될거야"}
